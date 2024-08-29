4a38cd9dc80bda99160a6e1b32ad21bd6005574e
sahil77vishwakarma <sahil777vishwakarma@gmail.com>
2024-08-30 01:40:27 +0530

    changes in AddDoctorActivity

--- app/src/main/java/com/example/mintlifesciences/addDoctor/AddDoctorActivity.kt
+++ app/src/main/java/com/example/mintlifesciences/addDoctor/AddDoctorActivity.kt
@@ -18,97 +18,86 @@ import com.example.mintlifesciences.R
 import com.example.mintlifesciences.databinding.ActivityAddDoctorBinding
 import com.example.mintlifesciences.homescreen.HomeActivity
 import com.google.android.material.textfield.TextInputEditText
+import org.w3c.dom.Text
 
 class AddDoctorActivity : AppCompatActivity() {
-
-    lateinit var binding:ActivityAddDoctorBinding
+    lateinit var binding: ActivityAddDoctorBinding
     private lateinit var viewModel: AddDoctorViewModel
-    private lateinit var adapter:AddDoctorAdapter
+    private lateinit var adapter: AddDoctorAdapter
     private lateinit var selectedItem: String
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_doctor)
         window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_VISIBLE
-
         supportActionBar?.setDisplayShowTitleEnabled(false)
-        binding=DataBindingUtil.setContentView(this,R.layout.activity_add_doctor)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_add_doctor)
+
         //binding.btn.setOnClickListener(this)
-        binding.recDocView.layoutManager=LinearLayoutManager(this)
-        viewModel=ViewModelProvider(this)[AddDoctorViewModel::class.java]
+        binding.recDocView.layoutManager = LinearLayoutManager(this)
+        viewModel = ViewModelProvider(this)[AddDoctorViewModel::class.java]
         viewModel.init(this)
+
         selectedItem = intent.getStringExtra("SELECTED_ITEM") ?: ""
 
-        adapter = AddDoctorAdapter(emptyList())
+        adapter = AddDoctorAdapter(this, emptyList(), selectedItem)
         binding.recDocView.adapter = adapter
         binding.recDocView.layoutManager = LinearLayoutManager(this)
 
-
-        binding.backArrow.setOnClickListener{
+        binding.backArrow.setOnClickListener {
             onBackPressed()
             true
         }
 
-
-
+        viewModel.loadDoctorData(selectedItem)
         viewModel.loadDoctorData(selectedItem)
 
         viewModel.docData.observe(this, Observer { doctors ->
-            Log.d("AddDoctorActivity", "Received data: $doctors")
+            Log.d("MainActivity", "Received data: $doctors")
             adapter.updateList(doctors)
         })
 
-
-        binding.btn.setOnClickListener{
-
+        binding.btn.setOnClickListener {
             showDoctorDialog()
         }
-
-
     }
+
     override fun onBackPressed() {
         super.onBackPressed()
+        // Navigate back to MainActivity
         val intent = Intent(this, HomeActivity::class.java)
         startActivity(intent)
-        finish()
+        finish() // Destroy SecondActivity when navigating back to MainActivity
     }
 
     private fun showDoctorDialog() {
+        val dialogView = LayoutInflater.from(this).inflate(R.layout.dialog_add_doctor, null)
 
+        dialogView.setOnClickListener {
+            val intent = Intent(this, DoctorMedicineActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
 
-
-       val dialogView=LayoutInflater.from(this).inflate(R.layout.dialog_add_doctor,null)
-
-//        dialogView.setOnClickListener{
-//            val intent=Intent(this, DoctorMedicineActivity::class.java)
-//            startActivity(intent)
-//            finish()
-//        }
-
-        val docName=dialogView.findViewById<TextInputEditText>(R.id.doc_edit)
-        val docSpec=dialogView.findViewById<TextInputEditText>(R.id.spec_edit)
-
+        val docName = dialogView.findViewById<TextInputEditText>(R.id.doc_edit)
+        val docSpec = dialogView.findViewById<TextInputEditText>(R.id.spec_edit)
         val dialog = AlertDialog.Builder(this)
             // .setTitle("Add Person")
             .setView(dialogView)
-          //  .setNegativeButton("Cancel", null)
+            //  .setNegativeButton("Cancel", null)
             .create()
 
-        dialogView.findViewById<AppCompatButton>(R.id.cnfrmBtn).setOnClickListener{
-            val name=docName.text.toString()
-            val speciality=docSpec.text.toString()
-
-                            if (name.isEmpty() || speciality.isEmpty() ) {
-                    Toast.makeText(this, "Please fill in all fields", Toast.LENGTH_SHORT).show()
-                    return@setOnClickListener
-                }
-           val doctor=DoctorData(name,speciality)
-            viewModel.saveDoctorData(selectedItem,doctor)
-            viewModel.addDoctor(DoctorData(name,speciality))
+        dialogView.findViewById<AppCompatButton>(R.id.cnfrmBtn).setOnClickListener {
+            val name = docName.text.toString()
+            val speciality = docSpec.text.toString()
+            if (name.isEmpty() || speciality.isEmpty()) {
+                Toast.makeText(this, "Please fill in all fields", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            val doctor = DoctorData(name, speciality)
+            viewModel.saveDoctorData(selectedItem, doctor)
+            viewModel.addDoctor(DoctorData(name, speciality))
             dialog.dismiss()
         }
         dialog.show()
-
     }
 }
